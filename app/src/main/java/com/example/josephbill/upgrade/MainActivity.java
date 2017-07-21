01ca4fda2be88d25edc9e646e215fb22c37ede29
josephbill <josephbill>
2017-07-21 16:12:51 +0300

    modifications

--- app/src/main/java/com/example/josephbill/upgrade/MainActivity.java
+++ app/src/main/java/com/example/josephbill/upgrade/MainActivity.java
@@ -88,6 +88,8 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
+        }else if(id == R.id.map){
+
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
